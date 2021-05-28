761352f7f628b2d8ce145041b8b25d1ac4d0d2c6
AnhTuanIT <ngocdungbb1@gmail.com>
2021-05-28 20:41:57 +0700

    config UI

--- src/main/java/com/se/photocopyshop/config/WebMVCConfig.java
+++ src/main/java/com/se/photocopyshop/config/WebMVCConfig.java
@@ -1,6 +1,7 @@
 package com.se.photocopyshop.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,10 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        // home
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("/user-page/index");
+
+        // login
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
         WebMvcConfigurer.super.addViewControllers(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
