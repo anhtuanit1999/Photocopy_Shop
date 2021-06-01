ef4346cf4e027296bdba405d5760b380d2ed271c
AnhTuanIT <ngocdungbb1@gmail.com>
2021-06-01 08:39:54 +0700

    thymeleaf config

--- src/main/java/com/se/photocopyshop/config/WebMVCConfig.java
+++ src/main/java/com/se/photocopyshop/config/WebMVCConfig.java
@@ -8,20 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("home").setViewName("/customer-page/index");
-//        registry.addViewController("").setViewName("/customer-page/index");
-//
-//        registry.addViewController("item-detail").setViewName("/customer/item_detail");
-//        registry.addViewController("profile/customer").setViewName("/customer/profile-cus");
-//        registry.addViewController("cart").setViewName("/customer/shoppingcart");
-//
-//
-//        registry.addViewController("sanpham").setViewName("/admin/tables");
-//        registry.addViewController("taikhoan").setViewName("/admin/taikhoan-khachhang");
-//        registry.addViewController("home/admin").setViewName("/admin/home");
-//        registry.addViewController("login").setViewName("login");
-//        registry.addViewController("profile/admin").setViewName("/admin/profile");
-//        registry.addViewController("form-user").setViewName("/admin/form-user");
+        registry.addViewController("home").setViewName("/customer-page/index");
+        registry.addViewController("").setViewName("/customer-page/index");
+
+        registry.addViewController("item-detail").setViewName("/customer/item_detail");
+        registry.addViewController("profile/customer").setViewName("/customer/profile-cus");
+        registry.addViewController("cart").setViewName("/customer/shoppingcart");
+
+
+        registry.addViewController("sanpham").setViewName("/admin/tables");
+        registry.addViewController("taikhoan").setViewName("/admin/taikhoan-khachhang");
+        registry.addViewController("home/admin").setViewName("/admin/home");
+        registry.addViewController("login").setViewName("login");
+        registry.addViewController("profile/admin").setViewName("/admin/profile");
+        registry.addViewController("form-user").setViewName("/admin/form-user");
         WebMvcConfigurer.super.addViewControllers(registry);
     }
 }
